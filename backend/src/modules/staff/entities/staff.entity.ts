831cb98daca9c67a61378fe32a8d20321a107f1e
Alikuxac <lexson270400@gmail.com>
2022-11-25 16:10:57 +0700

    chore: remove staffrole & cascade hotel

--- backend/src/modules/staff/entities/staff.entity.ts
+++ backend/src/modules/staff/entities/staff.entity.ts
@@ -9,7 +9,6 @@ import {
   ManyToOne,
 } from 'typeorm';
 
-// import { StaffRole } from './staff_role.entity';
 import { User } from '@modules/user/entities/user.entity';
 import { Hotel } from '@modules/hotel/entities/hotel.entity';
 
@@ -21,20 +20,12 @@ export class Staff {
   @Column({ name: 'start_date', type: 'date', nullable: false })
   startDate: Date;
 
-  // @ManyToOne(() => StaffRole, (staffRole) => staffRole.staff, {
-  //   eager: true,
-  //   cascade: true,
-  //   onDelete: 'SET NULL',
-  // })
-  // role: StaffRole;
-
   @OneToOne(() => User, (user) => user.staff)
   @JoinColumn({ referencedColumnName: 'id' })
   user: User;
 
   @ManyToOne(() => Hotel, (hotel) => hotel.staffs, {
     nullable: true,
-    cascade: true,
     onDelete: 'SET NULL',
   })
   hotel: Hotel;
