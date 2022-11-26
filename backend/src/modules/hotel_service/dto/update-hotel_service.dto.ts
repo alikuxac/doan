b5c0aac39746eb89e4406041e90e378bd36a42da
Alikuxac <lexson270400@gmail.com>
2022-11-26 15:42:47 +0700

    chore: remove hotel_service folder

--- backend/src/modules/hotel_service/dto/update-hotel_service.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateHotelServiceDto } from './create-hotel_service.dto';
-
-export class UpdateHotelServiceDto extends PartialType(CreateHotelServiceDto) {}
--- backend/src/modules/hotel_service/hotel_service.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HotelServiceService } from './hotel_service.service';
-import { HotelServiceController } from './hotel_service.controller';
-
-@Module({
-  controllers: [HotelServiceController],
-  providers: [HotelServiceService],
-})
-export class HotelServiceModule {}
