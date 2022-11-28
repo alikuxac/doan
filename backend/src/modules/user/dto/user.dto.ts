b515df5506db12cb8e2f65dda7c848b0800e9e2a
Alikuxac <lexson270400@gmail.com>
2022-11-28 10:57:56 +0700

    feat(user): add api property

--- backend/src/modules/user/dto/user.dto.ts
+++ backend/src/modules/user/dto/user.dto.ts
@@ -7,35 +7,68 @@ import {
   IsEnum,
 } from 'class-validator';
 
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 import { UserRole } from '../user.enum';
 
 export class createUserDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    required: true,
+    description: 'Username for user',
+    name: 'username',
+  })
   username: string;
 
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({ required: true, description: 'Email for user', name: 'email' })
   email: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    required: true,
+    description: 'First name of user',
+    name: 'firstName',
+  })
   firstName: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    required: true,
+    description: 'Last name of user',
+    name: 'lastName',
+  })
   lastName: string;
 
   @IsString()
   @IsOptional()
+  @ApiPropertyOptional({
+    description: 'Phone number of user',
+    name: 'phone',
+  })
   phone?: string;
 
   @IsString()
   @IsOptional()
+  @ApiPropertyOptional({
+    description: 'Password for user',
+    name: 'password',
+    default: null,
+  })
   password?: string;
 
   @IsEnum(UserRole)
   @IsOptional()
+  @ApiPropertyOptional({
+    description: 'Role for user',
+    enum: UserRole,
+    enumName: 'User role',
+    example: UserRole.USER,
+  })
   role: UserRole;
 }
 
@@ -48,5 +81,10 @@ export class updateUserDto extends PartialType(createUserDto) {
 export class assignRoleDto {
   @IsEnum(UserRole)
   @IsNotEmpty()
+  @ApiPropertyOptional({
+    description: 'Role for user',
+    enum: UserRole,
+    enumName: 'User role',
+  })
   role: UserRole;
 }
--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -7,14 +7,18 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import { ApiTags, ApiParam, ApiBody } from '@nestjs/swagger';
+
 import { UserService } from './user.service';
 import { assignRoleDto, createUserDto, updateUserDto } from './dto/user.dto';
 
 @Controller('user')
+@ApiTags('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiBody({ type: createUserDto })
   async create(@Body() dto: createUserDto) {
     return await this.userService.create(dto);
   }
@@ -25,21 +29,25 @@ export class UserController {
   }
 
   @Get(':id')
+  @ApiParam({ name: 'id' })
   async findOne(@Param('id') id: string) {
     return await this.userService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiParam({ name: 'id' })
   async update(@Param('id') id: string, @Body() dto: updateUserDto) {
     return await this.userService.update(+id, dto);
   }
 
   @Delete(':id')
+  @ApiParam({ name: 'id' })
   async remove(@Param('id') id: string) {
     return await this.userService.remove(+id);
   }
 
   @Post(':id/role')
+  @ApiParam({ name: 'id' })
   async changeRole(@Param('id') id: string, @Body() dto: assignRoleDto) {
     return await this.userService.changeRole(+id, dto);
   }
