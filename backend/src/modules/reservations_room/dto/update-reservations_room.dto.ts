15222581a4e607fc5ddb5c94a04bd23054080149
Alikuxac <lexson270400@gmail.com>
2022-11-28 22:02:58 +0700

    chore: remove reservation folder

--- backend/src/modules/reservations_room/dto/update-reservations_room.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReservationsRoomDto } from './create-reservations_room.dto';
-
-export class UpdateReservationsRoomDto extends PartialType(
-  CreateReservationsRoomDto,
-) {}
--- backend/src/modules/reservations_room/reservations_room.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ReservationsRoomService } from './reservations_room.service';
-import { ReservationsRoomController } from './reservations_room.controller';
-
-@Module({
-  controllers: [ReservationsRoomController],
-  providers: [ReservationsRoomService],
-})
-export class ReservationsRoomModule {}
